1080b49d04bd48bb15d9ad5a2d09803c6f1f95d7
sntran <github@sntran.com>
2026-06-04 20:33:06 -0500

    build: set up deps and elixir version gate

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Ector.MixProject do
     [
       app: :ector,
       version: "0.1.0",
-      elixir: "~> 1.20-rc",
+      elixir: "~> 1.20",
       start_permanent: Mix.env() == :prod,
       deps: deps()
     ]
@@ -21,8 +21,11 @@ defmodule Ector.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+      {:ecto_sql, "~> 3.14.0"},
+      {:postgrex, "~> 0.22.2"},
+      {:ecto_sqlite3, "~> 0.24.0"},
+      {:stream_data, "~> 1.3", only: :test},
+      {:benchee, "~> 1.5", only: [:dev, :test]}
     ]
   end
 end
